5aefa98528d7d87b7721cd6cb18ff129add83160
Elvis Rahman <elvis.rahman@grabtaxi.com>
2024-01-14 17:00:09 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import InlineSVG from 'react-inlinesvg';
+import appLogo from './assets/images/acecare_logo.png'
+import Home from './views/home/home';
+const { Header, Content, Footer } = Layout;
+const items = [
+  {
+    key: "home",
+    label: "Home",
+  },
+  {
+    key: "about",
+    label: "About",
+  },
+  {
+    key: "doctors ",
+    label: "Doctors",
+  },
+  {
+    key: "departments ",
+    label: "Departments",
+  },
+  {
+    key: "contactUs",
+    label: "Contact Us",
+  },
+  {
+    key: "book",
+    label: "Book",
+  }
+]
+// const items = new Array(3).fill(null).map((_, index) => ({
+//   key: String(index + 1),
+//   label: `nav ${index + 1}`,
+// }));
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header className='header-style'>
+        <div className="app-logo"> 
+          <img src={appLogo} alt='app-logo' className='app-logo-img'/>
+          <scan className='app-logo-title'>Acecare</scan>
+        </div>
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          className='navbar-style'
+        />
+      </Header>
+      <Content className='content-style'>
+        <Home/>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
-}
-
+};
 export default App;
---
+++ src/views/home/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css'
+
+function Home(props) {
+    return (
+        <>
+            <section class="home">
+                <div class="info">
+                    <h1>Multi-Specialty Clinic</h1>
+                    <h3>Comprehensive care for all ages</h3>
+                    <p>Ace Care is a trusted multi-specialty clinic in Barpeta Road, Assam, offering a wide range of services for patients of all ages. Visit us for expert care and personalized treatment.</p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Home;
